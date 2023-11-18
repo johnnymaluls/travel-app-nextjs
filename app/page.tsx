88ac33500675bc383bf969576974f56bb0b19bfd
johnny <jmalulan7@gmail.com>
2023-11-18 16:31:03 +0800

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
 }
