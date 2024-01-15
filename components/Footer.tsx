1e61c877876b072da7222cae52dcf3da47602232
johnny <jmalulan7@gmail.com>
2024-01-15 20:07:52 +0800

    Add footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,79 @@
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  );
+};
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex justify-center items-center mb-24">
+      <div className="mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 flex w-full flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href="/" className="mb-10">
+            <Image src="hilink-logo.svg" alt="logo" height={74} width={29} />
+          </Link>
+
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((columns, index) => (
+              <FooterColumn key={index} title={columns.title}>
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
+                  {columns.links.map((link, index) => (
+                    <Link href="/" key={index}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link, index) => (
+                  <Link
+                    key={index}
+                    href="/"
+                    className="flex gap-4 md:flex-col lg:flex-row"
+                  >
+                    <p className="whitespace-nowrap">{link.label}:</p>
+                    <p className="medium-14 whitespace-nowrap text-blue-70">
+                      {link.value}
+                    </p>
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={SOCIALS.title}>
+                <ul className="regular-14 flex gap-4 text-gray-30">
+                  {SOCIALS.links.map((link, index) => (
+                    <Link key={index} href="/">
+                      <Image src={link} alt="logo" height={24} width={24} />
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
+          </div>
+        </div>
+        <div className="border bg-gray-20" />
+        <p className="regular-14 w-full text-center text-gray-30">
+          2024 | All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
